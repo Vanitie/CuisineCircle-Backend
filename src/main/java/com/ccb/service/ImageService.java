b4c37e0e8d0d710e7ca281d9a9e40acd0283a8c3
D-O-Derer <137024567+D-O-Derer@users.noreply.github.com>
2024-06-10 01:56:27 +0800

    Changes

--- src/main/java/com/ccb/service/ImageService.java
+++ src/main/java/com/ccb/service/ImageService.java
@@ -4,5 +4,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.ccb.model.pojo.Image;
 
 public interface ImageService extends IService<Image> {
-    public Image getImageByUrl(String url);
+    Image getImageByUrl(String url);
 }
