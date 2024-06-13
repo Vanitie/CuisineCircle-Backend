f9760d89c544c11f8e660184d5325078d0fcd722
D-O-Derer <137024567+D-O-Derer@users.noreply.github.com>
2024-06-13 20:32:12 +0800

    Changes

--- src/main/java/com/ccb/service/DishService.java
+++ src/main/java/com/ccb/service/DishService.java
@@ -2,10 +2,6 @@ package com.ccb.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.ccb.model.pojo.Dish;
-import com.ccb.model.pojo.DishComment;
-import com.ccb.model.pojo.Preference;
-import com.ccb.model.pojo.Tag;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
